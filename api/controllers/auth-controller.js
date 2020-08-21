142f04a9694845f5870450bf82b2b25f40aca5bb
Majid Joseph <majid.joseph7@gmail.com>
2020-08-20 23:20:55 -0400

    added jwt auth

--- api/controllers/auth-controller.js
+++ api/controllers/auth-controller.js
@@ -1,28 +1,38 @@
 const User = require('../models/user-model');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.loginUser = async (req, res) => {
-    const {email, password} = req.body;
-    
+    const { email, password } = req.body;
 
-    // const user = await new User({
-    //     first_name: "majid",
-    //     last_name: "joseph",
-    //     email: email
-    // }).save();
+    const foundUser = (await User.find({ email: email }))[0];
+    if (!foundUser) {
+        return res.status(400).json('Email or password incorrect');
+    }
+
+    const valid = await bcrypt.compare(password, foundUser.password);
+    if (!valid) {
+        return res.status(400).json('Email or password incorrect');
+    }
+
+    res.cookie('token', generateJWT(foundUser._id), {
+        httpOnly: true,
+        secure: false,
+        sameSite: "none"
+    });
 
     res.sendStatus(200);
 }
 
 exports.registerUser = async (req, res) => {
 
-    const {first_name, last_name, email, password} = req.body;
-    
-    const existingUser = await User.find({email: email});
-    if(existingUser.length !== 0){
+    const { first_name, last_name, email, password } = req.body;
+
+    const existingUser = (await User.find({ email: email }))[0];
+    if (existingUser) {
         return res.status(400).json('Email already exists');
     }
-    
+
     const salt = await bcrypt.genSalt(10);
     const encryptedPassword = await bcrypt.hash(password, salt);
 
@@ -33,5 +43,21 @@ exports.registerUser = async (req, res) => {
         password: encryptedPassword
     }).save();
 
+    res.cookie('token', generateJWT(newUser._id), {
+        httpOnly: true,
+        secure: false,
+        sameSite: "none"
+    });
+
+    console.log(newUser._id);
+
     res.sendStatus(200);
+}
+
+generateJWT = (ID) =>{
+    const payload = {
+        userId: ID
+    };
+
+    return jwt.sign(payload, process.env['secret'], {expiresIn: "0.25hr"});
 }
